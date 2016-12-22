34f2da08d41e279e5c0a36111f4468afcbac2c0b
khan_luo <luo_hiter@126.com>
2016-12-22 15:26:47 +0800

    add jam_pulse

--- jam_pulse.m
+++ jam_pulse.m
@@ -1,13 +1,20 @@
-clc;
-clear;
-Ac=1;
-fs=3e9;
-fc=2e8;
-fp=2e7;
-dt=1/fs;
-t=0:1500;
-b=randi(2,1,15)-1
-% ur=Ac*sin(2*pi*fc*t/fs).*square(2*pi*fp*t,5);
-ur=square(2*pi*t,5);
-plot(ur); 
- 
+function [ur]=jam_pulse(Ac,fs,fc,N)
+%     Ac=1;
+%     fs=3e9;
+%     fc=2e8;
+%     N=15000;
+    n=15;
+    b=randi(2,1,n)-1;
+    N=N/n;
+    N
+    ur=zeros(1,N);
+    % ur=Ac*sin(2*pi*fc*t/fs).*square(2*pi*fp*t,5);
+    for t=1:15
+        T=(t-1)*N+1:t*N;
+        if b(t)==1
+            ur(T)=Ac*sin(2*pi*fc*T/fs);
+        else
+            ur(T)=0;
+        end
+    end
+
