d15cf7936f2ee0c7320eb83ff77158872f6f879a
ER Lu <luo_hiter@126.com>
2017-04-19 12:08:25 +0800

    add test_filter.m

--- test_filter.m
+++ test_filter.m
@@ -1,24 +1,24 @@
-fs=8e9;
-N=10240;
+Ac=1;
+fs=9e9;
+N=1500;
 t=1:N;
-ff1=1.9e9;
-ff2=3.05e9;
-ff3=3.3e9;
 
-f1=2.9e9;
-f3=3.1e9;
-fs1=2.89e9;
-fsh=3.19e9;
+fc=3.354e9;
+f1=3.2e9;
+f3=3.4e9;
+step=20;
+fstep=(f3-f1)/step;
 
-x=sin(2*pi*ff2*t/fs);
+%x=jam_zaidai(Ac,fs,N);
+x=sin(2*pi*fc/fs*t);
 xr=awgn(x,-5,'measured','db');
 figure(1);
 subplot(211);plot(t,xr);
 subplot(212);hua_fft(xr,fs,1);
 % y=filter(bz1,az1,x);
 
-for i=1:20 
-    y=bandp(xr,f1+i*1e7,f1+(i+1)*1e7,f1+(i-0.5)*1e7,f1+(i+1.5)*1e7,0.1,30,fs);
+for i=1:step
+    y=bandp(xr,f1+i*fstep,f1+(i+1)*fstep,f1+(i-0.5)*fstep,f1+(i+1.5)*fstep,0.1,30,fs);
     if i==1
         temp=sum(y.^4,2)/N;
     elseif temp<(sum(y.^4,2)/N)
@@ -28,6 +28,26 @@ for i=1:20
 %     figure(2);
 %     subplot(211);plot(t,y);
 %     subplot(212);hua_fft(y,fs,1);
-    f1+tempi*1e7
 end
+    f1+tempi*fstep
+f1=f1+(tempi-1)*fstep;
+f3=f1+2*fstep;
+fstep=(f3-f1)/step;
 
+en=zeros(1,step);
+for i=1:step
+    y=bandp(xr,f1+i*fstep,f1+(i+1)*fstep,f1+(i-0.5)*fstep,f1+(i+1.5)*fstep,0.1,30,fs);
+    if i==1
+        temp=sum(y.^4,2)/N;
+    elseif temp<(sum(y.^4,2)/N)
+        temp=(sum(y.^4,2)/N);
+        tempi=i;
+    end 
+    en(i)=temp;
+%     figure(2);
+%     subplot(211);plot(t,y);
+%     subplot(212);hua_fft(y,fs,1);
+end
+    f1+(tempi+0.5)*fstep
+    
+    
