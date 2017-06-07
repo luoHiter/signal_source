bbae326319745455d28e4e7ee87536e29bdf9ec4
khan_luo <luo_hiter@126.com>
2017-06-07 12:59:21 +0800

    en update

--- energy_detect.m
+++ energy_detect.m
@@ -1,44 +1,50 @@
 clc;
 clear;
 tic;
-f0=3e9;
-fs=8e9;
+fc=3e9;
+fs=12e9;
+fd=12e7;
 N=1500;
 pf=0.01;
 %varn=A/snr;
 varn=1;
 t=0:N-1;
 N1=0;N2=0;
-N_sample=10000;
-
+N_sample=1000;
+N_code=15;
 begin_N=1500;
-end_N=4500;
-dN=1000;
+end_N=6000;
+dN=1500;
 begin_snr=-18;
-end_snr=0;
-
+end_snr=15;
+jsr=20;
 
 pdd=zeros(1,end_snr-begin_snr+1);
 k=begin_snr:end_snr;
 for N=begin_N:dN:end_N
     signal_noi_1= zeros(N_sample,N);
     signal_noi_2= zeros(N_sample,N);
+    N_code=N/100;
     for snr = begin_snr:end_snr 
         N1=0;
         for i=1:N_sample
             A=sqrt(2)*varn*10^(snr/20);
-
+            As=A*10^(-jsr/20);
+         
             %signal=A*sin(2*pi*f0*t/fs);
-            signal=LFM(A,fs,N);
+            jar=LFM(A,fs,N);
+            signal=bpsk(N_code,fc,fs,fd,As);
             zaosheng=wgn(1,N,varn,'linear');
-            signal_noi_1(i,:)=signal+zaosheng;
+            varns=sum(signal.^2,2)/N;
+            
+            signal_noi_1(i,:)=jar+zaosheng+signal;
 
-            signal_noi_2(i,:)=wgn(1,N,varn,'linear'); 
+            signal_noi_2(i,:)=wgn(1,N,varn,'linear')+signal; 
         end
 
         pow_1=sum(signal_noi_1.^2,2)/N;
         pow_2=sum(signal_noi_2.^2,2)/N; 
-        lambda=varn*(N+sqrt(2*N)*qfuncinv(pf))/N;
+        lambda=(varn+varns)*(N+sqrt(2*N)*qfuncinv(pf))/N;
         for i=1:N_sample
             if(pow_1(i)>lambda)
                 N1=N1+1;
@@ -56,18 +62,18 @@ for N=begin_N:dN:end_N
     if N==1500
         plot(k,pdd,':or');
         hold on;
-    elseif N==2500
+    elseif N==3000
         plot(k,pdd,':dm');
         hold on;
-    elseif N==3500
+    elseif N==4500
         plot(k,pdd,':hb');
         hold on;
-    elseif N==4500
+    elseif N==6000
         plot(k,pdd,':.k');
         hold on;
     end         
 end
-legend('N=1500','N=2500','N=3500','N=4500'); 
+legend('N=1500','N=3000','N=4500','N=6000'); 
 ylabel('Pd');
 xlabel('SNR/dB');
 title('能量检测算法不同情况下检测概率');
