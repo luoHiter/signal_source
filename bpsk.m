7408ac2a173e7f8ce4b405dd2484545f703b0bbf
khan_luo <luo_hiter@126.com>
2017-06-07 13:08:27 +0800

    bpsk

---
+++ bpsk.m
@@ -0,0 +1,15 @@
+function y=bpsk(N_code,fc,fs,fd,Ac)
+    x=randi([0,1],1,N_code);
+    N=length(x);
+    nsamp=fs/fd;
+    for j=1:N
+        if x(j)==0
+           for i=1:nsamp
+             y((j-1)*nsamp+i)=Ac*cos(2*pi*fc*(i-1)/fs);
+           end
+        else
+           for i=1:nsamp
+             y((j-1)*nsamp+i)=Ac*cos(2*pi*fc*(i-1)/fs+pi);
+            end     
+        end
+end
